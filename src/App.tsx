d005cb734ecf01f39e03a0cbcf41fc1e303ff815
sfarwreurkah <sawerafarrukh99@gmail.com>
2025-01-19 14:41:54 +0500

    cart navbar video 44

--- src/App.tsx
+++ src/App.tsx
@@ -1,120 +1,18 @@
-
-//  video 40   updating objects
-
-// import { useState } from "react";
-
-// function App() {
-//     const [customer, setCustomer] = useState({
-//         name: 'jhn',
-//         address:{
-//             city: 'manestor',
-//             zipcode: '77665'
-//         }
-//     });
-
-//     const handleClick = () => {
-//         setCustomer({
-//             ...customer, 
-//             address: {...customer.address , zipcode: '12345'},
-//         });
-//     };
-
-//     return (
-//         <div>
-//             <button onClick={handleClick}>Click me</button>
-//         </div>
-//     );
-// }
-
-// export default App;
- 
-
-
-
-// -----------------------------------------------------------------------------------
-
-//  video 41    updating arrays
-
-// import { useState } from "react";
-
-// function App() {
-//     const [tags, setTags] = useState(['happy', 'cheerful']);
-
-//     const handleClick = () => {
-//         setTags([...tags, 'exciting']);
-
-//         setTags(tags.filter(tag => tag !== 'happy'));
-
-//         setTags(tags.map(tag => tag === 'happy' ? 'happiness' : tag));
-//     };
-
-//     return (
-//         <div>
-//             <button onClick={handleClick}>Click me</button>
-//         </div>
-//     );
-// }
-
-// export default App;
-
-
-
-// -----------------------------------------------------------------------------------
-
-//  video 42    updating arrays part 2
-
-// import { useState } from "react";
-
-// function App() {
-//     const [bugs, setBugs] = useState([
-//         { id: 1, title: 'Bug 1', fixed: false },
-//         { id: 2, title: 'Bug 2', fixed: false },
-//     ]);
-
-//     const handleClick = () => {
-//         setBugs(bugs.map(bug => bug.id === 1 ? { ...bug, fixed: true } : bug));
-//     };
-
-//     return (
-//         <div>
-//             <button onClick={handleClick}>Click me</button>
-//         </div>
-//     );
-// }
-
-// export default App;
-
-
-// -----------------------------------------------------------------------------------
-
-//  video 43    updating arrays  ( working with immer)
-
 import { useState } from "react";
-import produce from "immer";
+import Navbar from "./components/NavBar";
+import Cart from "./components/Cart";
 
-function App() {
-    const [bugs, setBugs] = useState([
-        { id: 1, title: 'Bug 1', fixed: false },
-        { id: 2, title: 'Bug 2', fixed: false },
-    ]);
-
-    const handleClick = () => {
-        setBugs(produce(draft => {
-            const bug = draft.find(bug => bug.id === 1);
-            if (bug) bug.fixed = true;
-        }));
-    };
+function App(){
+    const [cartItems, setCartItems] = useState(['product1' , 'prouct2'])
 
     return (
-        <div>
-            {bugs.map(bug => (
-                <p key={bug.id}>
-                    {bug.title} {bug.fixed ? 'Fixed' : 'New'}
-                </p>
-            ))}
-            <button onClick={handleClick}>Click me</button>
-        </div>
+   <div>
+    <Navbar cartItemsCount={cartItems.length } />
+    <Cart cartItems= {cartItems} onClear={() => setCartItems ([])} /> 
+   </div>
+
     );
 }
+
 
 export default App;
