39544af533fd2a206863395a083a68796d028b3e
sfarwreurkah <sawerafarrukh99@gmail.com>
2025-01-20 15:57:56 +0500

    From component from 48 to 56

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,9 @@
-//  video 45 
+import Form from "./components/Form";
 
-// import { useState } from "react";
-
-// function App() {
-//     const [game, setGame] = useState({
-//         id: 1,
-//         player: {
-//             name: 'john',
-//         },
-//     });
-
-//     const handleClick = () => {
-//         setGame({
-//             ...game,
-//             player: { ...game.player, name: 'bob' }
-//         });
-//     };
-
-//     return (
-//         <div>
-//             <button onClick={handleClick}>Change Name</button>
-//             <p>Player Name: {game.player.name}</p>
-//         </div>
-//     );
-// }
-
-// export default App;
-
-
-
-// --------------------------------------------------------------------------------------------------------
-
-//  video 46 
-
-
-import ExpandableText from "./components/ExpandableText";
-
-function App() {
-
-    return (
-        <div><ExpandableText maxChars={10}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-         Exercitationem odit maxime quos, inventore soluta perferendis at odio
-          autem porro reprehenderit eum, magnam commodi sint voluptatum consectetur
-           perspiciatis eaque quas. Ullam?
-        
-        
-                </ExpandableText>
-                
-                </div>
-    );
+function App(){
+    return(
+        <div>
+            <Form/>
+        </div>
+    )
 }
-
-export default App;
